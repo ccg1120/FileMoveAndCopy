48e2f27a492dcb2a8eb51ba9c642fa5164f06000
ch.choi <ccg.1120@daum.net>
2019-09-25 19:05:58 +0900

    1. 불필요 코드 삭제 (json 기능 이전)

--- FileUtility/FileUtility/FileUtility/StartSetting.cs
+++ FileUtility/FileUtility/FileUtility/StartSetting.cs
@@ -16,32 +16,6 @@ namespace FileUtility
         public string Version;
         [DataMember]
         public bool IsLoadStartSetting;
-
-        public static string GetJsonString(StartSetting setting)
-        {
-            MemoryStream str = new MemoryStream();
-
-            var ser = new DataContractJsonSerializer(typeof(StartSetting));
-
-            ser.WriteObject(str, setting);
-
-            byte[] jsondyte = str.ToArray();
-
-            str.Close();
-            string result = Encoding.UTF8.GetString(jsondyte, 0, jsondyte.Length);
-            Console.WriteLine(result);
-            return result;
-        }
-
-        public static StartSetting ReadToStartSetting(string json)
-        {
-            var deserializedUser = new StartSetting();
-            var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
-            var ser = new DataContractJsonSerializer(deserializedUser.GetType());
-            deserializedUser = ser.ReadObject(ms) as StartSetting;
-            ms.Close();
-            return deserializedUser;
-        }
     }
 
 
